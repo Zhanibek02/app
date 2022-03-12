ed9d1494d4e5f9812b7534ed7a9810e67c35c076
Zhanibek <bzr.kz@mail.ru>
2022-03-13 02:14:58 +0600

    My com

---
+++ main.py
@@ -0,0 +1,21 @@
+class Bird:
+    def _init_(self, range_flight, flight_altide, stomach_capacity):
+        self.range_flight = range_flight
+        self.flight_altitude = flight_altide
+        self.stomach_capacity = stomach_capacity
+
+def display_max_range(self):
+    #Отабразим макс дальность полета
+    print(f"На данный момент максимальная дальность полета: {self.range_flight}")
+
+def set_max_range(self, range_flight):
+    #Устанавливаем макс дальность полета
+    self.range.flight = range_flight
+
+def display_max_altitude(self):
+    #отабразим макс высоту
+    print(f"На данный момент птичка находится на высоте: {self.flight_altitude}")
+
+def set_max_flight_altitude(self, flight_altitude):
+    #устанавливаем макс высоту полета
+    self.flight_altitude = flight_altitude
